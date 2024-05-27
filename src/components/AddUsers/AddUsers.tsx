3243b390896b3163c8a6c94cce16826e552f5504
praveen1508 <47203110+praveen1508@users.noreply.github.com>
2024-05-27 20:54:42 +0530

    Async Validation

--- src/components/AddUsers/AddUsers.tsx
+++ src/components/AddUsers/AddUsers.tsx
@@ -1,28 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { UserModel } from '../../utils/Interfaces/UserModel'
 import { ErrorMessage, Field, Form, Formik } from 'formik'
 import * as Yup from 'yup';
 import './AddUsers.scss';
 
 export const AddUsers = () => {
-  const intialValues: UserModel = {
-    name: 'Ram',
-    age: 20,
-    gender: 'Male'
+  const [userData,setUserData]= useState<UserModel> ();
+  const fetchUserData = async () => {
+    const response = await fetch('http://localhost:3000/users');
+    const data: UserModel[] = await response.json();
+    setUserData(data[0]);
   }
+  
+  useEffect(() => {
+    fetchUserData();
+  },[]);
 
   const onSubmit = (values: UserModel) => {
     console.log(values);
   }
 
+  const isNameExists = async (value: string) => {
+    const response = await fetch('http://localhost:3000/users');
+    const data: UserModel[] = await response.json();
+    //False shows Error, so return false if the name exists
+    console.log('sdfew'+data.some((user) => user.name === value));
+    return data.some((user) => user.name === value)? false : true;
+  }
+
   const validationSchema = Yup.object({
-    name: Yup.string().min(3, 'Min 3 Chars').required('Field is Required'),
+    name: Yup.string().min(3, 'Min 3 Chars').required('Field is Required').test('Unique Name','Name Already Exists',  (value) => isNameExists(value)),
     age: Yup.number().max(50, 'Max Value is 50').required('Field is Required'),
     gender: Yup.string().min(3, 'Min 3 Chars').required('Field is Required'),
   })
 
   return (
-    <Formik initialValues={intialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+    userData &&
+    <Formik initialValues={userData} onSubmit={onSubmit} validationSchema={validationSchema}>
       <Form>
         <div>
           Name:
